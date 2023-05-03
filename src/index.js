255288b482ba422b82f4b072e291f7e923395958
ColdBlood237 <115800548+ColdBlood237@users.noreply.github.com>
2023-05-03 21:12:32 +0200

    Make the units change when user clicks on temperature

--- src/index.js
+++ src/index.js
@@ -4,18 +4,34 @@ import weather_data from "./weather_data";
 
 const city_input = document.getElementById("city-input");
 const city_form = document.getElementById("city-form");
+const temp = document.querySelector(".temp");
+
+let city_weather;
+let current_unit = "celsius";
 
 window.onload = async () => {
-  let city_weather = weather_data();
+  city_weather = weather_data();
   await city_weather.fetch_weather_data("Turin");
   console.log(JSON.stringify(city_weather.data));
-  render_weather(city_weather.data);
+  render_weather(city_weather.data, current_unit);
 };
 
 city_form.onsubmit = async (e) => {
   e.preventDefault();
-  let city_weather = weather_data();
+  city_weather = weather_data();
   await city_weather.fetch_weather_data(city_input.value);
-  render_weather(city_weather.data);
+  render_weather(city_weather.data, current_unit);
   city_form.reset();
 };
+
+temp.onclick = () => {
+  if (current_unit === "celsius") {
+    current_unit = "fahrenheit";
+    render_weather(city_weather.data, current_unit);
+  } else if (current_unit === "fahrenheit") {
+    current_unit = "celsius";
+    render_weather(city_weather.data, current_unit);
+  }
+};
+
+function switch_unit() {}
--- src/render_data.js
+++ src/render_data.js
@@ -6,15 +6,21 @@ const feels_like = document.querySelector(".feels-like");
 const wind = document.querySelector(".wind");
 const humidity = document.querySelector(".humidity");
 
-function render_weather(weather_data) {
+function render_weather(weather_data, unit) {
   if (Object.keys(weather_data).length !== 0) {
     condition_text.innerHTML = `${weather_data.condition_text}`;
     location.innerHTML = `${weather_data.city}, ${weather_data.country}`;
-    condition_icon.src = weather_data.condition_icon;
-    temp.innerHTML = `${weather_data.temp_c}<sup>°C</sup>`;
-    feels_like.innerHTML = `FEELS LIKE: ${weather_data.feels_like_c}<sup>°C</sup>`;
-    wind.innerHTML = `WIND: ${weather_data.wind_kph} KPH`;
     humidity.innerHTML = `HUMIDITY: ${weather_data.humidity}%`;
+    condition_icon.src = weather_data.condition_icon;
+    if (unit === "celsius") {
+      temp.innerHTML = `${weather_data.temp_c}<sup>°C</sup>`;
+      feels_like.innerHTML = `FEELS LIKE: ${weather_data.feels_like_c}<sup>°C</sup>`;
+      wind.innerHTML = `WIND: ${weather_data.wind_kph} KPH`;
+    } else if (unit === "fahrenheit") {
+      temp.innerHTML = `${weather_data.temp_f}<sup>°F</sup>`;
+      feels_like.innerHTML = `FEELS LIKE: ${weather_data.feels_like_f}<sup>°F</sup>`;
+      wind.innerHTML = `WIND: ${weather_data.wind_mph} MPH`;
+    }
   }
 }
 
